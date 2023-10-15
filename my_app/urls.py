a2bc7e3cfcd7f2d3dcbe31d277086d219b66d516
Peter Mwendia <ptermwendia@gmail.com>
2023-10-15 21:24:35 +0300

    user module

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('daraja/stk-push', views.stk_push_callback, name='mpesa_stk_push_callback'),
     path('transactions/', views.transaction_list, name='transaction_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'), 
+
 
 ]
