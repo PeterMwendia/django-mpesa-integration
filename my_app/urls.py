b9319b9d0eae902ed997bd4801251c199da1ad87
Peter Mwendia <ptermwendia@gmail.com>
2023-10-15 21:55:40 +0300

    Update urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('daraja/stk-push', views.stk_push_callback, name='mpesa_stk_push_callback'),
     path('transactions/', views.transaction_list, name='transaction_list'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('users/', views.view_all_users, name='view_all_users'),
  
