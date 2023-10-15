8c4ed11b408ba03c8e6bacb74ffe3036d20e6b09
Peter Mwendia <ptermwendia@gmail.com>
2023-10-15 21:11:25 +0300

    Update models.py

--- my_app/models.py
+++ my_app/models.py
@@ -9,9 +9,32 @@ class User(AbstractUser):
         ('F', 'Female'),
         ('O', 'Other'),
     )
+    username = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     phone_number = models.CharField(max_length=15)
 
+class Token(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    merchant_request_id = models.CharField(max_length=255)
+    checkout_request_id = models.CharField(max_length=255)
+    result_code = models.IntegerField()
+    result_desc = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    mpesa_receipt_number = models.CharField(max_length=255)
+    transaction_date = models.CharField(max_length=255)
+    
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('TP', 'Token Purchase'),
+        # Add other transaction types here
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_date = models.DateTimeField()
+    transaction_type = models.CharField(max_length=2, choices=TRANSACTION_TYPES)
+    result_code = models.IntegerField()
+    result_desc = models.CharField(max_length=255)
