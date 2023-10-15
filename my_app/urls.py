8daf50e1839234d4b49b4db487dcd14be8ec5402
Peter Mwendia <ptermwendia@gmail.com>
2023-10-15 21:19:45 +0300

    Update urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('daraja/stk-push', views.stk_push_callback, name='mpesa_stk_push_callback'),
+    path('transactions/', views.transaction_list, name='transaction_list'),
+
 ]
