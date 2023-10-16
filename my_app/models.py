0d6daf68e1fb325903d4342d31d08bd784e80a64
Peter Mwendia <ptermwendia@gmail.com>
2023-10-16 07:58:19 +0300

    Update models.py

--- my_app/models.py
+++ my_app/models.py
@@ -1,34 +1,19 @@
 from django.db import models
 
 # Create your models here.
-# from django.contrib.auth.models import AbstractUser
-
-# class User(AbstractUser):
-#     GENDER_CHOICES = (
-#         ('M', 'Male'),
-#         ('F', 'Female'),
-#         ('O', 'Other'),
-#     )
-#     username = models.CharField(max_length=30)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     email = models.EmailField(max_length=254)
-#     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-#     phone_number = models.CharField(max_length=15)
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, password=None):
+        extra_fields = {}
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -37,7 +22,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser):
     GENDER_CHOICES = (
@@ -58,11 +43,11 @@ class CustomUser(AbstractBaseUser):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'gender', 'phone_number']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.email
+        return self.username
 
 
 class Token(models.Model):
